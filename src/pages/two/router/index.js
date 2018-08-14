989efead481ea8bf315b638caeac08dc99436e24
antqi <hi.antqi@gmail.com>
2018-08-14 10:53:58 +0800

    feat:多路由

--- src/pages/two/router/index.js
+++ src/pages/two/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '../components/HelloWorld'
+import Hello from '../components/hello'
+import World from '../components/world'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: Hello
+    },
+    {
+      path: '/hello',
+      component: Hello
+    },
+    {
+      path: '/world',
+      component: World
     }
   ]
 })
+
+export default router
